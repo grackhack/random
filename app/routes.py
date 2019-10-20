9fdecc173224ab9e395bf4751c6ae751d1c329e2
GrackHack <grackhack@gmail.com>
2019-10-20 16:35:40 +0300

    last res

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, flash, redirect, url_for
 
 from app import app
 from app.forms import LoginForm
+from app.models import Game
 
 
 @app.route('/')
@@ -18,7 +19,8 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    games = Game.query.all()
+    return render_template('index.html', title='Home', user=user, posts=posts, games=games)
 
 
 @app.route('/login', methods=['GET', 'POST'])
