c67d517a3cb0b5ef236e7326d0023250ae37b180
GrackHack <grackhack@gmail.com>
2019-10-20 18:21:34 +0300

    tmp

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL') or 'postgresql+psycopg2://postgres:postgress@localhost:5432/dev_db'
+        'DATABASE_URL') or 'postgresql://postgres:postgress@localhost:5432/dev_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'not today'
