a24bf66a0d3d6b453e95adc8cdb7f5a1795fb33c
GrackHack <grackhack@gmail.com>
2019-12-14 20:40:01 +0300

    refresh page

--- app/static/js/common.js
+++ app/static/js/common.js
@@ -2,7 +2,9 @@ function get_games() {
     $.post('/collect_games', {
                 data: 12
             }).done(function(response) {
-                console.log(response['data'])
+                location.reload();
+                $("#msg").addClass("alert alert-success");
+                $("#msg").html("<p> Refresh done!</p>");
             }).fail(function() {
                 console.log('fail')
             });
