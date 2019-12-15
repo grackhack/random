03e2b2c0596fc8379cd3949af44ada7eac08aa00
GrackHack <grackhack@gmail.com>
2019-12-15 21:55:03 +0300

    style

--- app/filters.py
+++ app/filters.py
@@ -3,8 +3,8 @@ from app import app
 
 
 @app.template_filter('formatdatetime')
-def format_datetime(value, format="%d.%b_%H:%M"):
-    """Format a date time to (Default): d Mon YYYY HH:MM P"""
+def format_datetime(value, format="%H:%M"):
+    """Format a date time to (Default %d.%b_): d Mon YYYY HH:MM P"""
     if value is None:
         return ""
     return value.date.strftime(format)
